ec2a3499fe746bceb6dba46eee04123e1c710cec
timsuchanek <tim.suchanek@gmail.com>
2020-05-06 11:01:30 +0200

    simplify

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const path = require('path')
 const { Worker } = require('worker_threads')
 
-async function main() {
-  const worker = new Worker(path.join(__dirname, 'worker.js'))
-  worker.postMessage({})
-  worker.on('message', (message) => {
-    const hash = Buffer.from(message.value).toString('hex')
-    console.log(hash)
-    worker.unref()
-  })
-}
-
-main()
+const worker = new Worker(path.join(__dirname, 'worker.js'))
+worker.postMessage({})
+worker.on('message', (message) => {
+  const hash = Buffer.from(message.value).toString('hex')
+  console.log(hash)
+  worker.unref()
+})
